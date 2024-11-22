850f9a47fe0fc1bd8ece59e2b3b05b647dad91de
dianaortega1 <diana.ortega@clearviewlive.com>
2024-11-22 12:47:47 -0600

    fixed issue with repos locator

--- pages/training/github-user.ts
+++ pages/training/github-user.ts
@@ -1,56 +1,55 @@
 import { type Locator, type Page, expect } from "@playwright/test";
 
-export class GithubUser{
+export class GithubUser {
 
     //*Variables
-    readonly page : Page;
-    readonly repos : Locator;
-    readonly following : Locator; 
-    readonly user : Locator;
-    readonly searchBar : Locator;
-    readonly searchButton : Locator;
+    readonly page: Page;
+    readonly repos: Locator;
+    readonly following: Locator;
+    readonly user: Locator;
+    readonly searchBar: Locator;
+    readonly searchButton: Locator;
 
-    constructor (page: Page) {
+    constructor(page: Page) {
         this.page = page;
-        this.repos = page.locator('[data-testid="rate-limit"]');
+        this.repos = page.locator('.item > div > h3');
 
-        
         this.following = page.locator('//*[@id="root"]/main/section[2]/section/article[3]/div/h3');
         this.user = page.locator('//*[@id="root"]/main/section[3]/div/article[1]/header/div/p');
         this.searchBar = page.getByTestId('search-bar');
-        this.searchButton = page.getByRole( 'button', { name: 'Search' } );
+        this.searchButton = page.getByRole('button', { name: 'Search' });
     }
 
     //*Actions
-    async searchUser( name:string ) {
+    async searchUser(name: string) {
         await this.searchBar.fill(name);
         await this.searchButton.click();
     }
 
-    async fillSearchBar( name:string ) {
+    async fillSearchBar(name: string) {
         await this.searchBar.fill(name);
     }
 
-    async clickSearchButton(){
+    async clickSearchButton() {
         await this.searchButton.click();
     }
 
     //*Asserts
-    async repoChangedAfterSearch(){
-        
-        await this.repos.waitFor({ state: 'visible', timeout:30000 });
-        
-        const initialText = await this.repos.textContent();
+    async repoChangedAfterSearch() {
+        //await this.section.waitFor({ state: "attached", timeout: 60000 });
+        await this.repos.nth(1).waitFor({ state: 'visible', timeout: 30000 });
+
+        const initialText = await this.repos.nth(1).textContent();
 
         this.searchUser('test');
 
         await expect(this.repos.innerText()).not.toBe(initialText);
     }
-    async followingChangedAfterSearch(){
-        
+    async followingChangedAfterSearch() {
+
     }
-    async userChangedAfterSearch(){
-        
+    async userChangedAfterSearch() {
+
     }
 
 }
--- tests-training/github-user.spec.ts
+++ tests-training/github-user.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect, type Page } from '@playwright/test';
 import { GithubUser } from "../pages/training/github-user";
 
-//Setup
-
-
-
 test.describe('Github users search homepage', () => {
+    //Setup
     let githubUser: GithubUser;
     test.beforeEach(async ({ page }) => {
         await page.goto('https://gh-users-search.netlify.app/');
         githubUser = new GithubUser(page);
     });
     //Teardown
-    test( 'Assess repos change after a search', async () =>{
+    test.afterEach(async ({ page }) => {
+        await page.screenshot({ path: 'debug.png' });
+    })
 
+    test('Assess repos change after a search', async () => {
         githubUser.repoChangedAfterSearch();
 
     });
